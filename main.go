f15f27f2a118c86a5e1ca63a08caff0e6cf4779b
Tom Scott <tomscott87@gmail.com>
2016-10-01 17:24:43 +0100

    Reading from  config.toml

--- main.go
+++ main.go
@@ -5,53 +5,61 @@ import (
 	"net/http"
 	"sync"
 	"time"
+
+	"github.com/BurntSushi/toml"
+	"github.com/fatih/color"
 )
 
-type GetRequest struct {
-	url     string
-	timeout int // in milliseconds
+type tomlConfig struct {
+	Endpoints map[string]endpoint
+}
+
+type endpoint struct {
+	Name    string
+	URL     string
+	Timeout int
 }
 
 func main() {
-	var wg sync.WaitGroup
 
-	var gets = []GetRequest{
-		{url: "http://www.golang.org/", timeout: 1000},
-		{url: "http://www.google.com/", timeout: 1000},
-		{url: "http://www.somestupidname.com/", timeout: 1000},
-		{url: "http://www.openbet.com/", timeout: 1000},
-		{url: "http://www.ladbrokes.com/", timeout: 1000},
-		{url: "https://github.com/tscott0/saijgs", timeout: 1000},
-	}
+	color.Cyan("Prints text in cyan.")
 
-	for _, req := range gets {
-		// Increment the WaitGroup counter.
-		wg.Add(1)
-
-		// req will be overwritten. take a copy for each iteration
-		r := req
-
-		// Launch a goroutine to fetch the URL.
-		go r.hitURL(&wg)
-	}
-	// Wait for all HTTP fetches to complete.
-	wg.Wait()
-}
-
-func (r *GetRequest) hitURL(wg *sync.WaitGroup) {
-	// Decrement the counter when the goroutine completes.
-	defer wg.Done()
-
-	timeout := time.Duration(time.Duration(r.timeout) * time.Millisecond)
-	client := http.Client{
-		Timeout: timeout,
-	}
-	resp, err := client.Get(r.url)
-
-	if err != nil {
-		fmt.Println("Error while getting \"" + r.url + "\": " + err.Error())
+	var config tomlConfig
+	if _, err := toml.DecodeFile("config.toml", &config); err != nil {
+		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(resp.Status + " from " + r.url)
+	var wg sync.WaitGroup
+
+	for name, endpoint := range config.Endpoints {
+		fmt.Printf("Server: %s (%s, %s, %d)\n", name, endpoint.Name, endpoint.URL, endpoint.Timeout)
+		// Increment the WaitGroup counter.
+		wg.Add(1)
+		// req will be overwritten. take a copy for each iteration
+		e := endpoint
+		// Launch a goroutine to fetch the URL.
+		go e.hitURL(&wg)
+	}
+
+	wg.Wait()
+
+}
+
+func (e *endpoint) hitURL(wg *sync.WaitGroup) {
+	// Decrement the counter when the goroutine completes.
+	defer wg.Done()
+
+	timeout := time.Duration(time.Duration(e.Timeout) * time.Millisecond)
+	client := http.Client{
+		Timeout: timeout,
+	}
+	resp, err := client.Get(e.URL)
+
+	if err != nil {
+		fmt.Println("Error while getting \"" + e.URL + "\": " + err.Error())
+		return
+	}
+
+	fmt.Println(resp.Status + " from " + e.URL)
 }
